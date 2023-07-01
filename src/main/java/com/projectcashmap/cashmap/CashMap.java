eb97c9c5d813e04c7933f5fd5fc38bafdab6e7ed
Dulara00 <dularadinuli@gmail.com>
2023-07-01 17:21:31 +0530

    Add Project Files-Initials

---
+++ src/main/java/com/projectcashmap/cashmap/CashMap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.projectcashmap.cashmap;
+
+/**
+ *
+ * @author dular
+ */
+public class CashMap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
